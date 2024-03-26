b2d2965618bc370047fea6bb72ffda88dc96d0eb
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-26 19:49:59 -0300

    front end terminado

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', async function() {
+  const tbody = document.querySelector('#body-table');
+  const data = await fetch('../data/productos.json');
+  const response = await data.json();
+  console.log(response);
+
+  let contenidoTableBody = '';
+
+  response.forEach(element => {
+    // alt + 96
+    contenidoTableBody +=`
+      <tr>
+        <td>${element.id}</td>
+        <td>${element.nombre}</td>
+        <td>${element.precio}</td>
+        <td>${element.stock}</td>
+      </tr>
+    `;
+  })
+
+  console.log(contenidoTableBody);
+  tbody.innerHTML = contenidoTableBody;
+
+})
